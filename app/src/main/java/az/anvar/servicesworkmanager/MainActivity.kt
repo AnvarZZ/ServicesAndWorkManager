02d4d2cf927a6e08bbc8d6dbd7d394ac29638639
AnvarZZ <anvarzeynalzade@gmail.com>
2022-11-09 11:53:02 +0400

    Initial commit

---
+++ app/src/main/java/az/anvar/servicesworkmanager/MainActivity.kt
@@ -0,0 +1,17 @@
+package az.anvar.servicesworkmanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import az.anvar.servicesworkmanager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
