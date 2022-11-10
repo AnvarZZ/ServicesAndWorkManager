9960c73c1dc392df09509337bc192e63936b51ef
AnvarZZ <anvarzeynalzade@gmail.com>
2022-11-10 16:58:20 +0400

    JobService was created

---
+++ app/src/main/java/az/anvar/servicesworkmanager/MyJobService.kt
@@ -0,0 +1,43 @@
+package az.anvar.servicesworkmanager
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MyJobService : JobService() {
+
+    private val coroutine = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onStartJob")
+        coroutine.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer: $i")
+            }
+            jobFinished(params, true)
+        }
+        return true
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
+        coroutine.cancel()
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyService: $message")
+    }
+}
