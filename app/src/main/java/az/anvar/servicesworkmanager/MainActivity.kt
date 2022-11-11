b08e7e96cf54f11a46cde2ed1ad1a54dab4e55ef
AnvarZZ <anvarzeynalzade@gmail.com>
2022-11-11 13:20:12 +0400

    JobService 2

--- app/src/main/java/az/anvar/servicesworkmanager/MainActivity.kt
+++ app/src/main/java/az/anvar/servicesworkmanager/MainActivity.kt
@@ -1,5 +1,8 @@
 package az.anvar.servicesworkmanager
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -17,19 +20,27 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
-
         binding.foregroundService.setOnClickListener {
             ContextCompat.startForegroundService(
                 this,
                 MyForegroundService.newIntent(this)
             )
         }
-
         binding.intentService.setOnClickListener {
             ContextCompat.startForegroundService(
                 this,
                 MyIntentService.newIntent(this)
             )
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .build()
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
         }
     }
 }
