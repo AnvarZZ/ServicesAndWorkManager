bed577e8fe1d176489ad921cdc7e79507f72ccef
AnvarZZ <anvarzeynalzade@gmail.com>
2022-11-12 13:59:22 +0400

    JobIntentService

--- app/src/main/java/az/anvar/servicesworkmanager/MyJobIntentService.kt
+++ app/src/main/java/az/anvar/servicesworkmanager/MyJobIntentService.kt
@@ -2,24 +2,22 @@
 
 package az.anvar.servicesworkmanager
 
-import android.app.IntentService
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.JobIntentService
 
-class MyIntentService2 : IntentService(NAME) {
+class MyJobIntentService : JobIntentService() {
 
     @Deprecated("Deprecated in Java")
     override fun onCreate() {
         super.onCreate()
         log("onCreate")
-        setIntentRedelivery(true)
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onHandleIntent(intent: Intent?) {
+    override fun onHandleWork(intent: Intent) {
         log("onHandleIntent")
-        val page = intent?.getIntExtra(PAGE, 0) ?: 0
+        val page = intent.getIntExtra(PAGE, 0)
         for (i in 0 until 100) {
             Thread.sleep(1000)
             log("Timer: $i page: $page")
@@ -33,16 +31,25 @@ class MyIntentService2 : IntentService(NAME) {
     }
 
     private fun log(message: String) {
-        Log.d("SERVICE_TAG", "MyIntentService2: $message")
+        Log.d("SERVICE_TAG", "MyJobIntentService: $message")
     }
 
     companion object {
 
-        private const val NAME = "MyIntentService"
         private const val PAGE = "page"
+        private const val JOB_ID = 1
 
-        fun newIntent(context: Context, page: Int): Intent {
-            return Intent(context, MyIntentService2::class.java).apply {
+        fun enqueue(context: Context, page: Int) {
+            enqueueWork(
+                context,
+                MyJobIntentService::class.java,
+                JOB_ID,
+                newIntent(context, page)
+            )
+        }
+
+        private fun newIntent(context: Context, page: Int): Intent {
+            return Intent(context, MyJobIntentService::class.java).apply {
                 putExtra(PAGE, page)
             }
         }
